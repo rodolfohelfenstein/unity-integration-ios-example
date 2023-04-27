dc3617b388ed78cc8fea39fb99fa3c08a3400a74
Rodolfo Helfenstein Bulgam <rodolfo.helfenstein@MMB031152CPS.local>
2023-04-27 15:58:21 -0300

    Initial Commit

---
+++ UnityIntegrationApp/UnityIntegrationApp/Sources/Screens/Home/HomeView.swift
@@ -0,0 +1,39 @@
+//
+//  HomeView.swift
+//  UnityIntegrationApp
+//
+//  Created by Rodolfo Helfenstein Bulgam on 27/04/23.
+//
+
+import UIKit
+
+final class HomeView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Setup View
+    
+    private func setup() {
+        setupSubviews()
+        setupAnchors()
+        setupAppearance()
+    }
+    
+    private func setupSubviews() {
+        
+    }
+    
+    private func setupAnchors() {
+        
+    }
+    
+    private func setupAppearance() { }
+    
+}
---
+++ UnityIntegrationApp/UnityIntegrationApp/Sources/Screens/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  UnityIntegrationApp
+//
+//  Created by Rodolfo Helfenstein Bulgam on 27/04/23.
+//
+
+import UIKit
+
+final class HomeViewControlller: UIViewController {
+    
+    override func loadView() {
+        view = HomeView(frame: .zero)
+    }
+
+}
+
